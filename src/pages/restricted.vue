1a8b67ffb5514aec3f9453df2d0e90ec189af08c
NumiKun <nugrohorizki20@gmail.com>
2023-11-22 19:48:55 +0700

    init

---
+++ src/pages/restricted.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam deserunt quo sapiente quidem eos vero delectus dolorum dolorem distinctio, officia animi, nihil odit inventore ullam repudiandae enim! Quidem, voluptatibus libero?
+    </div>
+</template>
