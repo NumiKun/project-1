3732c1c3f1e099f71afb08b9d5a9b2e3dc386336
NumiKun <nugrohorizki20@gmail.com>
2023-11-23 18:34:16 +0700

    adding-login-logic

--- src/pages/restricted.vue
+++ src/pages/restricted.vue
@@ -1,5 +1,24 @@
 <template>
     <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam deserunt quo sapiente quidem eos vero delectus dolorum dolorem distinctio, officia animi, nihil odit inventore ullam repudiandae enim! Quidem, voluptatibus libero?
+      <div v-if="!auth.username">
+        <router-link to="/login">Silakan login untuk mengakses halaman ini</router-link>
+      </div>
+      <div v-else>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi assumenda vitae doloremque distinctio quos! Quam dolorem eligendi sit placeat numquam, eos voluptates animi officiis vitae, cupiditate exercitationem in unde nisi!
+      </div>
     </div>
-</template>
+  </template>
+  
+  <script setup lang="ts">
+  import { useAuthStore } from '@/stores/auth';
+  import { useRouter } from 'vue-router';
+  
+  const auth = useAuthStore();
+  const router = useRouter();
+
+  if (!auth.username) {
+    alert('Silakan login untuk mengakses halaman ini');
+    router.push('/login');
+  }
+  </script>
+  
