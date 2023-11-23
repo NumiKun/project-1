1a15942441ff0aeb26202bbc6393d7cd740e16f0
NumiKun <nugrohorizki20@gmail.com>
2023-11-23 18:39:17 +0700

    adding-login-logic

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -5,12 +5,19 @@
 
     const auth = useAuthStore()
     const username = ref('')
+    const password = ref('')
     const router = useRouter()
 
     const onLogin = () => {
-        auth.login(username.value)
+        const validUsername = 'admin'
+        const validPassword = '12345'
 
-        router.push('/')
+        if (username.value === validUsername && password.value === validPassword) {
+            auth.login(username.value)
+            router.push('/')
+        } else {
+            window.alert('Username atau password salah')
+        }
     }
 </script>
 
@@ -18,8 +25,9 @@
     <div>
         <h1>Login Page</h1>
         <div class="flex flex-col gap-2">
-            <input type="text" v-model="username" class="border p-2" />
-            <button @click="auth.login(username)" class="bg-blue-500 text-white py-1 px-3">Login</button>
+            <input type="text" v-model="username" class="border p-2" placeholder="Username" />
+            <input type="password" v-model="password" class="border p-2" placeholder="Password" />
+            <button @click="onLogin" class="bg-blue-500 text-white py-1 px-3">Login</button>
         </div>
     </div>
 </template>
